3c985db51e93f5adbf57c37b837f3d0594983003
alfredo <alvie97m@gmail.com>
2022-04-09 23:53:22 -0300

    adds tests to get followers functionality

--- handlers/community_handler_test.go
+++ handlers/community_handler_test.go
@@ -2,6 +2,7 @@ package handlers_test
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/jmoiron/sqlx"
@@ -12,6 +13,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"sort"
 	"testing"
 )
 
@@ -210,4 +212,107 @@ func TestCommunityHandler_UnfollowCommunity(ts *testing.T) {
 		}
 	})
 
+}
+
+func TestCommunityHandler_GetCommunityFollowers(t *testing.T) {
+
+	r := chi.NewRouter()
+	l := log.New(os.Stdout, "following-service-test", log.LstdFlags)
+	db := sqlx.MustConnect("sqlite3", ":memory:")
+	defer func(db *sqlx.DB) {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(db)
+	data.InitializeDB(db)
+
+	cr := data.NewCommunityRepositorySQL(l, db)
+	ur := data.NewUserRepositorySQL(l, db)
+	cs := services.NewCommunityService(l, cr, ur)
+	ch := handlers.NewCommunityHandler(l, cs)
+
+	const URL = communityFollowingsRoutePath
+	const URLFmt = communityFollowingsRoutePathFmt
+
+	r.Get(URL, ch.GetCommunityFollowers)
+
+	t.Run("test Get community followers", func(t *testing.T) {
+		cID := "1"
+		rUrl := fmt.Sprintf(URLFmt, cID)
+
+		req := httptest.NewRequest(http.MethodGet, rUrl, nil)
+		rr := httptest.NewRecorder()
+		r.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusOK {
+			t.Fatalf(
+				"http Code status not expected, expected %d got %d",
+				res.StatusCode,
+				http.StatusOK,
+			)
+		}
+
+		expected := []data.User{
+			{
+				ID:         1,
+				ExternalID: "1",
+			},
+			{
+				ID:         2,
+				ExternalID: "2",
+			},
+		}
+
+		var got handlers.FollowersResponse
+
+		if err := json.Unmarshal(rr.Body.Bytes(), &got); err != nil {
+			t.Fatal(err)
+		}
+
+		if len(got.Followers) != len(expected) {
+			t.Fatalf(
+				"followers list not expected, expected %d, got %d",
+				len(expected),
+				len(got.Followers),
+			)
+		}
+
+		sort.SliceStable(got.Followers, func(i, j int) bool {
+			return got.Followers[i].ID < got.Followers[j].ID
+		})
+
+		for i, user := range expected {
+			if user.ID != got.Followers[i].ID {
+				t.Fatalf(
+					"follower not expected.\nexpected:\n%#v\ngot:\n%#v",
+					user,
+					got.Followers[i],
+				)
+			}
+		}
+
+	})
+
+	t.Run("test Community Not found", func(t *testing.T) {
+
+		cID := "2"
+		rUrl := fmt.Sprintf(URLFmt, cID)
+
+		req := httptest.NewRequest(http.MethodGet, rUrl, nil)
+		rr := httptest.NewRecorder()
+		r.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusBadRequest {
+			t.Fatalf(
+				"http Code status not expected, expected %d got %d",
+				res.StatusCode,
+				http.StatusOK,
+			)
+		}
+	})
 }
