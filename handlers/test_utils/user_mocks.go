5ce595b08013ee0b097d096ff3f513fbccd4efe3
alfredo <alvie97m@gmail.com>
2022-04-10 21:23:43 -0300

    refactors user_handler_test.go

--- handlers/test_utils/user_mocks.go
+++ handlers/test_utils/user_mocks.go
@@ -21,6 +21,15 @@ var (
 		UserOne,
 		UserTwo,
 	}
+
+	CommunityOne = data.Community{
+		ID:         1,
+		ExternalID: "COMMUNITY-ONE",
+	}
+
+	CommunitiesList = []data.Community{
+		CommunityOne,
+	}
 )
 
 type UserServiceGetFollowersMock struct {
@@ -41,4 +50,138 @@ func (UserServiceGetFollowersErrorMock) GetUserFollowers(
 	*data.User,
 ) ([]data.User, error) {
 	return nil, errors.New("error")
+}
+
+type UserServiceGetCommunitiesMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceGetCommunitiesMock) GetUserCommunities(
+	*data.User,
+) ([]data.Community, error) {
+	return CommunitiesList, nil
+}
+
+type UserServiceGetCommunitiesErrorMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceGetCommunitiesErrorMock) GetUserCommunities(
+	*data.User,
+) ([]data.Community, error) {
+	return nil, errors.New("error")
+}
+
+type UserServiceUnfollowUserMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceUnfollowUserMock) GetUser(string) (*data.User, error) {
+	return &UserOne, nil
+}
+
+func (UserServiceUnfollowUserMock) UnfollowUser(*data.User, *data.User) error {
+	return nil
+}
+
+type UserServiceUnfollowUserNotFoundMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceUnfollowUserNotFoundMock) GetUser(string) (*data.User, error) {
+	return nil, data.ErrorUserNotFound
+}
+
+func (UserServiceUnfollowUserNotFoundMock) UnfollowUser(
+	*data.User,
+	*data.User,
+) error {
+	return nil
+}
+
+type UserServiceUnfollowUserGetUserErrorMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceUnfollowUserGetUserErrorMock) GetUser(string) (*data.User, error) {
+	return nil, errors.New("error")
+}
+
+func (UserServiceUnfollowUserGetUserErrorMock) UnfollowUser(
+	*data.User,
+	*data.User,
+) error {
+	return nil
+}
+
+type UserServiceUnfollowUserErrorMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceUnfollowUserErrorMock) GetUser(string) (*data.User, error) {
+	return &UserOne, nil
+}
+
+func (UserServiceUnfollowUserErrorMock) UnfollowUser(
+	*data.User,
+	*data.User,
+) error {
+	return errors.New("error")
+}
+
+type UserServiceFollowUserMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceFollowUserMock) GetUser(string) (*data.User, error) {
+	return &UserOne, nil
+}
+
+func (UserServiceFollowUserMock) FollowUser(*data.User, *data.User) error {
+	return nil
+}
+
+type UserServiceFollowUserNotFoundMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceFollowUserNotFoundMock) GetUser(string) (*data.User, error) {
+	return nil, data.ErrorUserNotFound
+}
+
+func (UserServiceFollowUserNotFoundMock) FollowUser(
+	*data.User,
+	*data.User,
+) error {
+	return nil
+}
+
+type UserServiceFollowUserGetUserErrorMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceFollowUserGetUserErrorMock) GetUser(string) (*data.User, error) {
+	return nil, errors.New("error")
+}
+
+func (UserServiceFollowUserGetUserErrorMock) FollowUser(
+	*data.User,
+	*data.User,
+) error {
+	return nil
+}
+
+type UserServiceFollowUserErrorMock struct {
+	services.UserServiceI
+}
+
+func (UserServiceFollowUserErrorMock) GetUser(string) (*data.User, error) {
+	return &UserOne, nil
+}
+
+func (UserServiceFollowUserErrorMock) FollowUser(
+	*data.User,
+	*data.User,
+) error {
+	return errors.New("error")
 }
